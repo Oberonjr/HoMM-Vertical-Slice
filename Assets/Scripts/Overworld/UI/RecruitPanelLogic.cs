c8a77639d30d246832aabc69d42108871a641e5a
Bulancea Matei <mateibogdanbulancea@gmail.com>
2024-12-09 23:43:36 +0100

    Fix dwelling weekly spawn

--- Assets/Scripts/Overworld/UI/RecruitPanelLogic.cs
+++ Assets/Scripts/Overworld/UI/RecruitPanelLogic.cs
@@ -66,11 +66,11 @@ public class RecruitPanelLogic : MonoBehaviour
         if (currentPlayer.Kingdom.Economy.CanSpendResource(ResourceData.ResourceType.Gold,
                 Mathf.RoundToInt(slider.value) * currentUnit.Cost)) //The check already happens in Spend(), but my sequencing is still currently all wrong
         {
+            OverworldEventBus<RecruitUnit>.Publish(new RecruitUnit(currentUnit, Mathf.RoundToInt(slider.value)));
             currentPlayer.Heroes[0].cHeroInfo.Army.AddUnit(currentUnit, Mathf.RoundToInt(slider.value));
             currentPlayer.Kingdom.Economy.SpendResource(ResourceData.ResourceType.Gold, Mathf.RoundToInt(slider.value) * currentUnit.Cost);
             slider.maxValue -= slider.value;
             availableText.text = "Available: " + slider.maxValue;
-            OverworldEventBus<RecruitUnit>.Publish(new RecruitUnit(currentUnit, Mathf.RoundToInt(slider.value)));
         }
         else
         {
